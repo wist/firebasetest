43bc49d042798e5dd690372f99b3e7b39ba92b20
Vitalii Istushkin <istushkin@gmail.com>
2016-09-11 23:05:57 +0200

    initial commit

--- Firebasetest/Firebasetest/ViewController.swift
+++ Firebasetest/Firebasetest/ViewController.swift
@@ -16,11 +16,68 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+ 
+    override func viewDidAppear(animated: Bool) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var emailFLD: UITextField!
+    @IBOutlet weak var pwdFLD: UITextField!
+    @IBOutlet weak var topLBL: UILabel!
+    @IBOutlet weak var botLBL: UILabel!
+    @IBOutlet weak var loginBTN: UIButton!
+    @IBOutlet weak var logoutBTN: UIButton!
+    @IBOutlet weak var regBTN: UIButton!
+    
+    @IBAction func didTapSignIn(sender: AnyObject) {
+        // Sign In with credentials.
+        let email = emailFLD.text
+        let password = pwdFLD.text
+        print("sign in start")
+        FIRAuth.auth()?.signInWithEmail(email!, password: password!) { (user, error) in
+            if let error = error {
+                print(error)
+                print(error.localizedDescription)
+                return
+            }
+             print("sign in ok")
+             print(user)
+             self.topLBL.text = "You have Signed In"
+  //          self.signedIn(user!)
+        }
+    }
+    
+    @IBAction func didTapSignUp(sender: AnyObject) {
+        let email = emailFLD.text
+        let password = pwdFLD.text
+        FIRAuth.auth()?.createUserWithEmail(email!, password: password!) { (user, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            print("sign UP ok")
+            print(user)
+            self.topLBL.text = "You have Signed UP"
+ //         self.setDisplayName(user!)
+        }
+    }
+    
+    
+    
+    @IBAction func signOut(sender: UIButton) {
+        let firebaseAuth = FIRAuth.auth()
+        do {
+            try firebaseAuth?.signOut()
+            dismissViewControllerAnimated(true, completion: nil)
+            print("sign out ok")
+            print(user)
+        } catch let signOutError as NSError {
+            print ("Error signing out: \(signOutError)")
+        }
+    }
+    
 }
-
