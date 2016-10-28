bada2d2e4253961719015a8e1c24a89d626145f3
Vitalii Istushkin <istushkin@gmail.com>
2016-10-28 23:06:33 +0200

    updated page view size

--- Firebasetest/Firebasetest/InViewController.swift
+++ Firebasetest/Firebasetest/InViewController.swift
@@ -83,8 +83,9 @@ class InViewController: UIViewController, UIPageViewControllerDataSource{
             
 
         
-        self.pageViewController.view.frame = CGRectMake(0, 30, self.view.frame.width, self.view.frame.size.height - 60)
-        
+//        self.pageViewController.view.frame = CGRectMake(0, 30, self.view.frame.width, self.view.frame.size.height - 60)
+
+    self.pageViewController.view.frame = CGRectMake(0, 80, 350, 450)
         
         
         self.addChildViewController(self.pageViewController)
